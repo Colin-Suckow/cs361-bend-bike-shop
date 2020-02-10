04e2d4e2f023a1d6d5eb543aa6190da40b7b2fb8
Colin Suckow <csuckow99@gmail.com>
2020-02-10 11:10:52 -0800

    Class refactor

--- bike.rb
+++ bike.rb
@@ -3,21 +3,28 @@
 class Bike
 
   STANDARD_WEIGHT = 200 # lbs
-  MAX_CARGO_ITEMS = 10
 
-  attr_accessor :id, :color, :price, :weight, :rented, :cargo_contents
+  attr_accessor :id, :color, :price, :weight, :rental, :cargo
 
-  def initialize(id, color, price, weight = STANDARD_WEIGHT, rented = false)
+  def initialize(id, color, price, weight = STANDARD_WEIGHT, rented = false, cargo = [])
     @id = id
     @color = color
     @price = price
     @weight = weight
-    @rented = rented
-    @cargo_contents = []
+    @rental = Rental.new(rented)
+    @cargo = Cargo.new(cargo)
   end
 
-  def rent!
-    self.rented = true
+end
+
+class Cargo
+  
+  MAX_CARGO_ITEMS = 10
+  
+  attr_accessor :cargo_contents
+
+  def initialize(cargo = [])
+    @cargo_contents = cargo
   end
 
   def add_cargo(item)
@@ -34,6 +41,20 @@ class Bike
 
   def pannier_remaining_capacity
     MAX_CARGO_ITEMS - self.cargo_contents.size
+  end
+
+end
+
+class Rental
+  
+  attr_accessor :rented
+
+  def initialize(rented = false)
+    @rented = rented
+  end
+
+  def rent!
+    self.rented = true
   end
 
 end
